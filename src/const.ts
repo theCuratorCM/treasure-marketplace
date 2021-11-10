4dfc7d681f75d1a87759fb108c6120ecc0f5284f
Yuya Oiwa <yuya.oiwa@me.com>
2021-11-10 12:18:51 +0900

    add fee under price for listing drawer

--- src/const.ts
+++ src/const.ts
@@ -39,3 +39,7 @@ export const collections = [
   },
   // { name: "Life", address: "#" },
 ] as const;
+
+export const DAO_SHARE = 0.025;
+export const CREATOR_SHARE = 0.025;
+export const USER_SHARE = 1 - DAO_SHARE - CREATOR_SHARE;
