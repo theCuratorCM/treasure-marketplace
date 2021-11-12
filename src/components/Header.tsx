9b5fa1d37494733011fdca13a3d3308b96721230
Yuya Oiwa <yuya.oiwa@me.com>
2021-11-12 17:49:46 +0900

    more banner fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -306,8 +306,8 @@ const Header = () => {
         {chainId !== ChainId.Arbitrum && (
           <div className="bg-yellow-600">
             <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-              <div className="flex items-center justify-between flex-wrap">
-                <div className="w-0 flex-1 flex items-center">
+              <div className="flex sm:items-center lg:justify-between flex-col space-y-2 sm:space-y-0 sm:flex-row">
+                <div className="flex-1 flex items-center">
                   <span className="flex p-2 rounded-lg bg-yellow-800">
                     <SpeakerphoneIcon
                       className="h-6 w-6 text-white"
@@ -318,27 +318,18 @@ const Header = () => {
                     <span className="lg:hidden">
                       Please switch to Arbitrum.
                     </span>
-                    <span className="hidden lg:inline">
+                    <span className="hidden lg:block">
                       You are currently on the {getChainName(chainId)} Network.
                       Please switch to Arbitrum.
                     </span>
                   </p>
                 </div>
-                <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+                <div className="flex-shrink-0 w-full sm:mt-0 sm:w-auto">
                   <button
                     onClick={switchToArbitrum}
                     className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-yellow-600 bg-white hover:bg-yellow-50"
                   >
                     Switch Networks
-                  </button>
-                </div>
-                <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-                  <button
-                    type="button"
-                    className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
-                  >
-                    <span className="sr-only">Dismiss</span>
-                    <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
                   </button>
                 </div>
               </div>
