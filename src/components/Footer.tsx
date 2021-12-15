fa79839ebec6943ff55bf7151b0e103346a77e36
Yuya Oiwa <yuya.oiwa@me.com>
2021-12-15 16:14:34 +0900

    footer width fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="py-10 flex items-center justify-between">
           <div className="flex items-center justify-center space-x-6 text-sm text-gray-500">
             {social.map((item) => (
